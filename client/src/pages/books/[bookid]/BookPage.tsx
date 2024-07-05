febf073238479d8b783442b4d80c1b12127be9fb
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-05 08:09:59 +0800

    added loading and toast for book page

--- client/src/pages/books/[bookid]/BookPage.tsx
+++ client/src/pages/books/[bookid]/BookPage.tsx
@@ -3,6 +3,8 @@ import { Link, useParams } from "react-router-dom";
 import { gql } from "../../../__generated__";
 import { GetBookQuery } from "../../../__generated__/graphql";
 import { useEffect, useState } from "react";
+import Throbber from "../../../components/Throbber";
+import { toast } from "react-toastify";
 
 const GET_BOOK = gql(`
   query GetBook($bookid: UUID!) {
@@ -34,16 +36,21 @@ const BookPage = () => {
   useEffect(() => {
     setBook(data?.book);
   }, [data]);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+
+  useEffect(() => {
+    if (error) toast.error(error.message);
+  }, [error]);
+
+  if (loading) return <Throbber />;
 
   return (
-    <section className="flex items-center justify-center">
+    <section className="flex items-center justify-center ">
       {book && (
-        <div className="max-w-2xl mx-auto p-6 bg-zinc-800 shadow-lg rounded-lg">
-          <h1 className="text-3xl font-bold mb-4 text-gray-200 text-center">
-            {book.name}
+        <div className="mx-auto p-6 bg-zinc-800 shadow-lg rounded-lg max-w-[500px] w-full">
+          <h1 className="text-3xl font-bold mb-4 text-gray-200">
+            📗 {book.name}
           </h1>
+          <div className="border-b border-zinc-700 mb-4"></div>
           <p className="text-sm text-gray-400 mb-2">ID: {book.id}</p>
           <p className="text-sm text-gray-400 mb-4">
             Created At: {new Date(book.createdAt).toLocaleString()}
@@ -51,7 +58,7 @@ const BookPage = () => {
 
           <Link to={`/user/${book.author.id}`}>
             <div className="hover:bg-zinc-600 mb-4 p-2 rounded-md">
-              <h2 className="text-xl font-semibold text-gray-200">Author</h2>
+              <h2 className="text-xl font-semibold text-gray-200">👤 Author</h2>
               <p className="text-gray-400">Name: {book.author.name}</p>
               <p className="text-sm text-gray-400 mb-4">ID: {book.author.id}</p>
             </div>
@@ -59,7 +66,9 @@ const BookPage = () => {
 
           <Link to={`/publishers/${book.publisher.id}`}>
             <div className="hover:bg-zinc-600 mb-4 p-2 rounded-md">
-              <h2 className="text-xl font-semibold text-gray-200">Publisher</h2>
+              <h2 className="text-xl font-semibold text-gray-200">
+                🏛️ Publisher
+              </h2>
               <p className="text-gray-400">Name: {book.publisher.name}</p>
               <p className="text-sm text-gray-400 ">ID: {book.publisher.id}</p>
             </div>
