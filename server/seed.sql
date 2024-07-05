b5e104e8ee3a1d52a951afec2cd63b9e0bd9f22c
mmvergara <mark.jesusmanabat@gmail.com>
2024-07-05 08:10:18 +0800

    refactor schema sql

--- server/seed.sql
+++ server/seed.sql
@@ -1,3 +1,31 @@
+drop table if exists public.books;
+drop table if exists public.publishers;
+drop table if exists public.users;
+
+
+create table
+  public.users (
+    id uuid not null default gen_random_uuid (),
+    username character varying not null,
+    password text not null,
+    created_at timestamp with time zone not null default now(),
+    constraint users_pkey primary key (id),
+    constraint users_username_key unique (username)
+  ) tablespace pg_default;
+
+
+
+create table
+public.publishers (
+  id uuid not null default gen_random_uuid (),
+  name text not null,
+  created_at timestamp with time zone not null default now(),
+  owner uuid not null,
+  constraint publisher_pkey primary key (id),
+  constraint publisher_name_key unique (name),
+  constraint publisher_owner_fkey foreign key (owner) references users (id) on update cascade on delete cascade
+) tablespace pg_default;  
+
 create table
   public.books (
     id uuid not null default gen_random_uuid (),
@@ -9,24 +37,4 @@ create table
     constraint books_author_fkey foreign key (author) references users (id) on update cascade on delete cascade,
     constraint books_publisher_fkey foreign key (publisher) references publishers (id) on update cascade on delete cascade
   ) tablespace pg_default;
-create table
-  public.publishers (
-    id uuid not null default gen_random_uuid (),
-    name text not null,
-    created_at timestamp with time zone not null default now(),
-    owner uuid not null,
-    constraint publisher_pkey primary key (id),
-    constraint publisher_name_key unique (name),
-    constraint publisher_owner_fkey foreign key (owner) references users (id) on update cascade on delete cascade
-  ) tablespace pg_default;
-create table
-  public.books (
-    id uuid not null default gen_random_uuid (),
-    name text not null,
-    created_at timestamp with time zone not null default now(),
-    author uuid not null,
-    publisher uuid not null,
-    constraint books_pkey primary key (id),
-    constraint books_author_fkey foreign key (author) references users (id) on update cascade on delete cascade,
-    constraint books_publisher_fkey foreign key (publisher) references publishers (id) on update cascade on delete cascade
-  ) tablespace pg_default;
+  
